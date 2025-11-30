5d751879b6295297233e2c3e54141712044ad02c
sleazy <nino.gabroshvili.4@iliauni.edu.ge>
2025-11-30 17:25:17 +0400

    header markup
    added headers components only markup no tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/icons/logo.svg";
+import search from "../assets/icons/search.svg";
+import instagram from "../assets/icons/instagram.svg";
+import facebook from "../assets/icons/facebook.svg";
+import telegram from "../assets/icons/telegram.svg";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div>
+        <img src={logo} alt="logo" />
+        <h1>Luminae</h1>
+      </div>
+      <div>
+        <div>
+          <input type="text" placeholder="Search Products" />
 
-export default Header
+          <select>
+            <option>All categories</option>
+          </select>
+
+          <button>
+            <img src={search} alt="search" />
+          </button>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <li>About us</li>
+          <li>Blog</li>
+          <li>Contact us</li>
+          <li>Help & support</li>
+        </ul>
+      </nav>
+
+      <nav>
+        <a href="#">
+          <img src={instagram} alt="" />
+        </a>
+        <a href="#">
+          <img src={facebook} alt="" />
+        </a>
+        <a href="#">
+          <img src={telegram} alt="" />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
