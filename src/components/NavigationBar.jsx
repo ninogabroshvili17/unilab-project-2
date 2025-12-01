80801efd8b13e0e5aea1e0ede9cf5fb4c30d626b
sleazy <nino.gabroshvili.4@iliauni.edu.ge>
2025-12-01 13:51:44 +0400

    navigation bar
    created a navigation bar under headers. active clicked page color may need to be updated later

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const navItems = [
+  "Woman",
+  "Male",
+  "Mother-Child",
+  "Home & Furniture",
+  "Supermarket",
+  "Cosmetics",
+  "Shoe & Bag",
+  "Electronic",
+  "Sport & Outdoor",
+  "Best seller",
+];
+
+const NavigationBar = () => {
+  return (
+    <nav className="h-20">
+      <ul className="max-w-[1260px] mx-auto h-full flex font-Lato text-white text-sm justify-between items-end pb-4">
+        {navItems.map((item) => (
+          <li key={item} className="cursor-pointer text-sm text-[#555555] ">
+            {item}
+          </li>
+        ))}
+        {/* #FF7A00 */}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header.jsx";
 import HeaderBottom from "../components/HeaderBottom.jsx";
+import NavigationBar from "../components/NavigationBar.jsx";
 
 const Home = () => {
   return (
     <>
       <Header />
       <HeaderBottom />
+      <NavigationBar />
     </>
   );
 };
