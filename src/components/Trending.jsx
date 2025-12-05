55fd6c79c4b32c38573c24242494db7e59777e01
sleazy <nino.gabroshvili.4@iliauni.edu.ge>
2025-12-05 14:49:37 +0400

    trending section finished
    trending section for desktop is finished. pictures from figma came with with new arivals logo.

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -17,11 +17,28 @@ const Trending = () => {
   return (
     <>
       <section className="font-Lato px-5">
-        <div className="max-w-[1440px] mx-auto grid grid-cols-3">
+        <div className="max-w-[1440px] mx-auto grid grid-cols-3 pb-20 gap-5">
           {trending.map((item) => {
             return (
-              <div key={item.id}>
-                <img src={item.image} alt="trending" />
+              <div className="rounded-lg overflow-hidden" key={item.id}>
+                <img
+                  className="cursor-pointer"
+                  src={item.image}
+                  alt="trending"
+                />
+                <div className="bg-[#262626] text-white flex justify-between items-center px-5 py-4.5">
+                  <div>
+                    <h5 className="pb-[13px] font-bold cursor-pointer">
+                      {item.title}
+                    </h5>
+                    <p className="text-sm text-[#C4C4C4] cursor-pointer">
+                      {item.brand}
+                    </p>
+                  </div>
+                  <button className="border border-white px-6 py-3 rounded-lg cursor-pointer">
+                    ${item.price} Shop Now
+                  </button>
+                </div>
               </div>
             );
           })}
